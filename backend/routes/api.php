60a6ab03a02b0c97442af10ecfcb22c272c01d33
Yehan20 <yehansk15@gmail.com>
2025-09-11 02:46:25 +0530

    feat(auth) add login logout user route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\api\AuthenticatedUserController;
+use App\Http\Middleware\EnsureUserIsGuest;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::middleware('throttle:api')->group(function () {
+
+
+    Route::post('login', [AuthenticatedUserController::class, 'store'])->middleware([EnsureUserIsGuest::class]);
+
+    Route::middleware(['auth:sanctum'])->group(function () {
+
+        Route::get('ping', function () {
+            return response()->json([
+                'message' => 'pong'
+            ]);
+        });
+
+        Route::post('logout', [AuthenticatedUserController::class, 'destroy']);
+        Route::get('user', [AuthenticatedUserController::class, 'show']);
+    });
+});
